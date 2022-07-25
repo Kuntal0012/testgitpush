e09bfe1ca6b3eb274c2234d9f5de00babdd04b9f
kuntal.roy <kuntal9804roy@gmail.com>
2022-07-25 18:26:02 +0530

    this is my first code push in git

---
+++ mongodbtest.py
@@ -0,0 +1,14 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Devil:k9804559492@devil.q5aaw.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+
+
+d = { "name" : "kuntal",
+    "email" : "kuntal@gmail.com",
+    "surname" : "roy"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
---
+++ test.py
@@ -0,0 +1,7 @@
+spam=int(input("enter the digit"))
+if spam == 1:
+    print("Hello")
+if spam == 2:
+    print("Howdy")
+else:
+    print("Greetings!")
